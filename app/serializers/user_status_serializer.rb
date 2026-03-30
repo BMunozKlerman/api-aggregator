51f083d846ff1ffdc024893fa3df1426b4dddef3
Benjamin Klerman Muñoz <benjamin.klerman@gmail.com>
2026-03-30 16:03:48 -0300

    DOC: add comment to user status serializer for clarity

--- app/serializers/user_status_serializer.rb
+++ app/serializers/user_status_serializer.rb
@@ -1,3 +1,4 @@
+# This serializer is responsible for defining how the user status data is represented in the API responses.
 class UserStatusSerializer < ActiveModel::Serializer
   attributes :id, :full_name, :experience, :pending_task_count, :next_urgent_task
 end
