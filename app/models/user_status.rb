31a34370f79fb72608fcdf1db3cd31095da58650
Benjamin Klerman Muñoz <benjamin.klerman@gmail.com>
2026-03-30 16:01:36 -0300

    DOC: add comment for user status model validations

--- app/models/user_status.rb
+++ app/models/user_status.rb
@@ -1,5 +1,6 @@
 # app/models/user_status.rb
 class UserStatus < ApplicationRecord
+  # Validations for the user status model
   validates :full_name, presence: true
   validates :experience, presence: true
   validates :pending_task_count, presence: true
